5f8eac91173b6615dec97e9e0990d3f829518f1c
leepuppychow <leepuppychow@gmail.com>
2017-12-27 17:19:21 -0700

    Can calculate Euclidean distance between two points (patients)

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,5 +4,13 @@ class Patient < ApplicationRecord
             :bodyweight,
             :knee_AROM,
             :classification,
-            :distance_to_unknown, presence: true  
+            :distance_to_unknown, presence: true
+
+  def find_distance_to(unknown)
+    self.distance_to_unknown = Math.sqrt(
+      (age - unknown.age) ** 2 +
+      (pain_level - unknown.pain_level) ** 2 +
+      (bodyweight - unknown.bodyweight) ** 2 +
+      (knee_AROM - unknown.knee_AROM) ** 2)
+  end
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -9,4 +9,14 @@ describe Patient, type: :model do
     it {is_expected.to validate_presence_of(:classification)}
     it {is_expected.to validate_presence_of(:distance_to_unknown)}
   end
+
+  describe "Instance Methods" do
+    it "can calculate Euclidean distance between two patients" do
+      patient = create(:patient)
+      unknown = create(:patient, age: 59, pain_level: 9)
+
+      patient.find_distance_to(unknown)
+      expect(patient.distance_to_unknown).to eq 5
+    end
+  end
 end
